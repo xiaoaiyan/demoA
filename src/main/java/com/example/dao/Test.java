0aa9d8e0aa61c638e42d97b421f426cc238a2a3c
server01 <370609265@qq,com>
2020-04-29 15:30:37 +0800

    注释

---
+++ src/main/java/com/example/dao/Test.java
@@ -0,0 +1,13 @@
+package com.example.dao;
+
+/**
+ * @author Mr.Xiao
+ * @date 2020/4/29 14:17
+ * @function 测试类注释
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
